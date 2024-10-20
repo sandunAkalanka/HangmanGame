52675541047e1c7990f2ed9b88d6bdb3f8c80a57
Sandun Akalanka <ggsakalanka@gmail.com>
2024-10-21 02:05:14 +0530

    Added another version

--- hangmanV2.py
+++ hangmanV2.py
@@ -1,6 +1,10 @@
+import random
+word_file = "wordlist.txt"
+WORD = open(word_file).read().splitlines()
+secret = random.choice(WORD)
+
 name = input("Enter name : ")
 print("Hello, "+ name + ". Welcome to Hangman Game!")
-secret = "apple"
 print("Your word has " , len(secret) , " characters. You will get 5 lives.")
 list1 = list(secret)
 list2 = ["_"] * len(list1)
@@ -9,6 +13,7 @@ lives = 5
 while lives >= 0 :
     if(lives == 0):
         print("Game over! Better luck next time!")
+        print("Word is " , secret)
         break
     if (list1 == list2):
         print("You won! Congratulations!")
@@ -25,3 +30,4 @@ while lives >= 0 :
         print("Incorrect guess!")
         lives -= 1
         print("You have " , lives , " lives left")
+        print("Word is ", list2)
